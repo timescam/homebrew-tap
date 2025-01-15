9f8e875c8800b0f5aee9c5e4a0e3902c5c6b5edd
timescam <timescam@duck.com>
2025-01-15 09:53:52 +0800

    update to reflect the version naming scheme

--- Casks/theboringnotch.rb
+++ Casks/theboringnotch.rb
@@ -1,8 +1,8 @@
 cask "theboringnotch" do
   version "jellyfin.snoring"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "97780877acdbecc6701fc5970478c708f36417b3c28c2fda3db07732645fdce0"
 
-  url "https://github.com/TheBoredTeam/boring.notch/releases/download/#{version}/#{version.split('-').map(&:capitalize).join}.dmg"
+  url "https://github.com/TheBoredTeam/boring.notch/releases/download/#{version}/#{version.split('.').map(&:capitalize).join}.dmg"
   name "TheBoringNotch"
   desc "Not so boring notch That Rocks 🎸🎶"
   homepage "https://github.com/TheBoredTeam/boring.notch"
