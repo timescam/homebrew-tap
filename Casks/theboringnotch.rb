95fd5769c8523f8967a718b78a2580f5eae04490
rexkyng <rexkyng@users.noreply.github.com>
2025-02-24 01:47:12 +0000

    chore: update TheBoringNotch to version wolf.painting

--- Casks/theboringnotch.rb
+++ Casks/theboringnotch.rb
@@ -1,6 +1,6 @@
 cask "theboringnotch" do
-  version "jellyfin.snoring"
-  sha256 "97780877acdbecc6701fc5970478c708f36417b3c28c2fda3db07732645fdce0"
+  version "wolf.painting"
+  sha256 "1a58ec27e5de30faf107fdf8b77575b1c39ace69e77b1330fc4ed6562bf2badc"
 
   url "https://github.com/TheBoredTeam/boring.notch/releases/download/#{version}/#{version.split('.').map(&:capitalize).join}.dmg"
   name "TheBoringNotch"
