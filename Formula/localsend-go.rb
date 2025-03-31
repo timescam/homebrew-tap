c61873d5a358e57409de05f409a5286fa7c64076
rexkyng <rex.ky.ng@pccw.com>
2025-03-31 10:29:10 +0800

    fix 404 due to dl package name change

--- Formula/localsend-go.rb
+++ Formula/localsend-go.rb
@@ -4,17 +4,17 @@ class LocalsendGo < Formula
   version "1.2.7"
 
   on_arm do
-    url "https://github.com/meowrain/localsend-go/releases/download/v#{version}/localsend_cli-darwin-arm64"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/meowrain/localsend-go/releases/download/v#{version}/localsend-go-darwin-arm64"
+    sha256 "2ee70ddde3e93a0401f36290a7294f6988595df2bb69edbd59d25ef6d2b623b9"
   end
 
   on_intel do
-    url "https://github.com/meowrain/localsend-go/releases/download/v#{version}/localsend_cli-darwin-amd64"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    url "https://github.com/meowrain/localsend-go/releases/download/v#{version}/localsend-go-darwin-amd64"
+    sha256 "0f796161068a558f74d71a599e76d8e98f6f95ca83a327fffb2ea0a5a0d2844a"
   end
 
   def install
-    binary_name = Hardware::CPU.arm? ? "localsend_cli-darwin-arm64" : "localsend_cli-darwin-amd64"
+    binary_name = Hardware::CPU.arm? ? "localsend-go-darwin-arm64" : "localsend-go-darwin-amd64"
     bin.install binary_name => "localsend-go"
   end
 
