93350a0089a3be1ff2b5553efd9314f567c0c82e
timescam <timescam@duck.com>
2025-04-10 12:40:25 +0800

    fix broken versioning auto update

--- Formula/pay-respects.rb
+++ Formula/pay-respects.rb
@@ -4,13 +4,13 @@ class PayRespects < Formula
   version "0.7.3"
 
   on_arm do
-    url "https://github.com/iffse/pay-respects/releases/download/v0.6.10/pay-respects-0.6.10-aarch64-apple-darwin.tar.zst"
-    sha256 "bd3c6269e56061ccbcba0fd763562b07cc18810510cc00c1451b789dfbcfe713"
+    url "https://github.com/iffse/pay-respects/releases/download/v0.7.3/pay-respects-0.7.3-aarch64-apple-darwin.tar.zst"
+    sha256 "efadb82414c29e58af9d846eed295ccaec3365582ae37f8e2a6fbb937ae1d549"
   end
 
   on_intel do
-    url "https://github.com/iffse/pay-respects/releases/download/v0.6.10/pay-respects-0.6.10-x86_64-apple-darwin.tar.zst"
-    sha256 "941c2823507fb9535a0e37722670170809a06aa2286f329e5e81bb17a22e265c"
+    url "https://github.com/iffse/pay-respects/releases/download/v0.7.3/pay-respects-0.7.3-x86_64-apple-darwin.tar.zst"
+    sha256 "59af7cd4d9208ee21bb4e79ae4a3a23ed86e729fd54268d1255699157ee8712b"
   end
 
   def install
