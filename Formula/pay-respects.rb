5fe6558723a81122b8de7fa53d251bafe969721e
rexkyng <rexkyng@users.noreply.github.com>
2025-04-23 01:20:43 +0000

    chore: update pay-respects to version 0.7.6

--- Formula/pay-respects.rb
+++ Formula/pay-respects.rb
@@ -1,16 +1,16 @@
 class PayRespects < Formula
   desc "CLI tool to pay respects"
   homepage "https://github.com/iffse/pay-respects"
-  version "0.7.5"
+  version "0.7.6"
 
   on_arm do
-    url "https://github.com/iffse/pay-respects/releases/download/v0.7.5/pay-respects-0.7.5-aarch64-apple-darwin.tar.zst"
-    sha256 "297b970d67b05bee4b1b451727cd348b89ccc4a16575e73f3014dfcc578220cb"
+    url "https://github.com/iffse/pay-respects/releases/download/v0.7.6/pay-respects-0.7.6-aarch64-apple-darwin.tar.zst"
+    sha256 "08fd0f83aa9bc4891710a8b0914d964c4be78c38e124300897891941ebc83de6"
   end
 
   on_intel do
-    url "https://github.com/iffse/pay-respects/releases/download/v0.7.5/pay-respects-0.7.5-x86_64-apple-darwin.tar.zst"
-    sha256 "e808fc2df9b1813f968d6d54621296f374f755d4ba9a85357d41523e39b5c546"
+    url "https://github.com/iffse/pay-respects/releases/download/v0.7.6/pay-respects-0.7.6-x86_64-apple-darwin.tar.zst"
+    sha256 "5d446d8338da3ccf33da0ac2ab57d0628adb080acdc0b8644840e63cdfeb41ee"
   end
 
   def install
