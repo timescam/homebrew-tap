2b145e5a1504db35125f22f4e404cd95a8b82185
timescam <timescam@duck.com>
2025-10-16 10:43:36 +0800

    audacity@nightly

---
+++ Casks/audacity@nightly.rb
@@ -0,0 +1,17 @@
+cask "audacity@nightly" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/timescam/homebrew-tap/releases/download/audacity-nightly/audacity-macOS-nightly-arm64.dmg"
+  name "Audacity"
+  desc "Audio editor"
+  homepage "https://www.audacityteam.org/beta/"
+
+  app "Audacity.app"
+
+  caveats <<~EOS
+    This build isn't signed; install with `--no-quarantine`, or after install run:
+    xattr -d com.apple.quarantine /Applications/Audacity.app
+    read more: https://support.apple.com/en-us/102445
+  EOS
+end
