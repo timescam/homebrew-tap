7b039e14309a2afa9bb45d4c2bd3b54b84770c31
rexkyng <rexkyng@users.noreply.github.com>
2026-02-15 19:21:19 +0000

    chore: update spotatui to version 0.36.2

--- Formula/spotatui.rb
+++ Formula/spotatui.rb
@@ -1,16 +1,16 @@
 class Spotatui < Formula
   desc "Spotify client for the terminal (Rust TUI, native streaming)"
   homepage "https://github.com/LargeModGames/spotatui"
-  version "0.36.1"
+  version "0.36.2"
 
   on_arm do
     url "https://github.com/LargeModGames/spotatui/releases/download/v#{version}/spotatui-macos-aarch64.tar.gz"
-    sha256 "9fecea5fc4e5458504da7b7ac7c5e629c5072fc4565cf1fed55860edfdb921b6"
+    sha256 "a20e4f9c60625a91bd6c2e7d8eb8f72e7689f6eb48761f414fcabfafd9189f8a"
   end
 
   on_intel do
     url "https://github.com/LargeModGames/spotatui/releases/download/v#{version}/spotatui-macos-x86_64.tar.gz"
-    sha256 "c152222892460c5da8b284efa0d8ac77d24bc3001c5545da38e7ae3d48ada685"
+    sha256 "c96b5ea6964aca71e4380315ad79ab515d543676a8326c3a777680120ceb92b0"
   end
 
   def install
