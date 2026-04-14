aebd8c43358ff2feec520195dbf072a3260b0d03
rexkyng <rexkyng@users.noreply.github.com>
2026-04-14 19:59:37 +0000

    chore: update koharu to version 0.44.1

--- Casks/koharu.rb
+++ Casks/koharu.rb
@@ -1,6 +1,6 @@
 cask "koharu" do
-  version "0.43.2"
-  sha256 "64213cb0fbbca5288d0d8b5f1f1cb01332fe2cdb1c773f5d6d650902c4ab10f7"
+  version ""
+  sha256 ""
 
   url "https://github.com/mayocream/koharu/releases/download/#{version}/koharu_#{version}_aarch64.dmg",
       verified: "github.com/mayocream/koharu/"
