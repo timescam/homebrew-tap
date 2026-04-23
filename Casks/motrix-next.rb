c222496e137e98488e48677f028f65e029dd5510
rexkyng <rexkyng@users.noreply.github.com>
2026-04-23 20:02:54 +0000

    chore: update motrix-next to version 3.8.2

--- Casks/motrix-next.rb
+++ Casks/motrix-next.rb
@@ -1,14 +1,14 @@
 cask "motrix-next" do
-  version "3.8.0"
+  version "3.8.2"
 
   on_arm do
-    sha256 "9ef8279bc6be80268ecfed50aa9cfe52e8d15c849d633b7c3530008fc68a1846"
+    sha256 "a62c57c6c98be07f71e1575a1dca3a9e51df70428989b80c7b7e5c1b072f30ad"
 
     url "https://github.com/AnInsomniacy/motrix-next/releases/download/v#{version}/MotrixNext_#{version}_aarch64.dmg",
         verified: "github.com/AnInsomniacy/motrix-next/"
   end
   on_intel do
-    sha256 "61dd9bdb9b5938313f7292631d39feaa8b0833e22864d999961d36a9e5680805"
+    sha256 "29a34d4bbf0fa1ca003581f4e61acc029c635a6c4fe937ce7deafa8953b3a70b"
 
     url "https://github.com/AnInsomniacy/motrix-next/releases/download/v#{version}/MotrixNext_#{version}_x64.dmg",
         verified: "github.com/AnInsomniacy/motrix-next/"
