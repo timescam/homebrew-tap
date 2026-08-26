1d56ddb11dfc9aa1bedb1d9305e621ec615c51ce
timescam <timescam@duck.com>
2026-08-26 10:06:14 +0800

    chore: peaclock use gcc@16

--- Formula/peaclock.rb
+++ Formula/peaclock.rb
@@ -4,18 +4,18 @@ class Peaclock < Formula
   head "https://github.com/octobanana/peaclock.git", branch: "master"
 
   depends_on "cmake" => :build
-  depends_on "gcc@14" => :build
+  depends_on "gcc@16" => :build
   depends_on "icu4c"
 
   def install
-    system "./RUNME.sh", "build", "--", 
-           "-DCMAKE_CXX_COMPILER=#{Formula["gcc@14"].opt_bin}/g++-14",
+    system "./RUNME.sh", "build", "--",
+           "-DCMAKE_CXX_COMPILER=#{Formula["gcc@16"].opt_bin}/g++-16",
            "-DCMAKE_CXX_FLAGS=-I#{Formula["icu4c"].opt_include} -L#{Formula["icu4c"].opt_lib}"
-    
+
     bin.install "build/release/peaclock"
   end
 
   test do
     system "#{bin}/peaclock", "--version"
   end
-end 
+end
