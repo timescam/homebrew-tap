a123c169e3a443c1ef11788fc9a5c66112ef4600
rexkyng <rexkyng@users.noreply.github.com>
2026-08-31 23:03:55 +0000

    chore: update koharu to version 0.80.0

--- Casks/koharu.rb
+++ Casks/koharu.rb
@@ -1,8 +1,8 @@
 cask "koharu" do
-  version "0.79.0"
-  sha256 "f13347b4c2a52932e4344295bffdc3c2e5dc4a78dad0c6b6fc6635249530bbe4"
+  version "0.80.0"
+  sha256 "dc5381d5d4b1dfe635efa7ae4953a64748d54dc2af8d3e200eef691d021d9d72"
 
-  url "https://github.com/mayocream/koharu/releases/download/0.79.0/koharu_0.79.0_aarch64.dmg",
+  url "https://github.com/koharu-rs/koharu/releases/download/0.80.0/koharu_0.80.0_aarch64.dmg",
       verified: "github.com/mayocream/koharu/"
   name "Koharu"
   desc "ML-powered manga translator"
