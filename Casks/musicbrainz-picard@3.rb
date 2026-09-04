ca7aaf9684e14d5b8d1cc333feb39b31d5d2093f
Rex Ng <timescam@duck.com>
2026-09-04 08:54:17 +0000

    musicbrainz-picard@3 3.0.0rc1

--- Casks/musicbrainz-picard@3.rb
+++ Casks/musicbrainz-picard@3.rb
@@ -1,13 +1,13 @@
 cask "musicbrainz-picard@3" do
-  version "3.0.0b9"
+  version "3.0.0rc1"
 
   on_arm do
-    sha256 "d95899bba9f79a17e955bfbf0776b162f546152ed877e06ef31fa05a0ca02738"
+    sha256 "d6241507fa977fa9b6c55d2d8f4259b4d1bdc8c05791ad8c3e330f8b55a4c67d"
 
     url "https://github.com/metabrainz/picard/releases/download/release-#{version}/MusicBrainz-Picard-#{version}-macOS-13.0-arm64.dmg"
   end
   on_intel do
-    sha256 "1ac5ffb09ad6293111a40dd9e70366d119d7cb44a862ac1ff4e91a89e5b22348"
+    sha256 "5b6af304c8b090c95c1ff25b8e7df333e2d494091146c4a7ce89cd7c50cb6fdb"
 
     url "https://github.com/metabrainz/picard/releases/download/release-#{version}/MusicBrainz-Picard-#{version}-macOS-13.0-x86_64.dmg"
   end
@@ -31,9 +31,8 @@ cask "musicbrainz-picard@3" do
     end
   end
 
+  conflicts_with cask: "musicbrainz-picard"
   depends_on macos: :ventura
-
-  conflicts_with cask: ["musicbrainz-picard"]
 
   app "MusicBrainz Picard.app"
 
@@ -43,5 +42,4 @@ cask "musicbrainz-picard@3" do
     "~/Library/Preferences/org.musicbrainz.picard.plist",
     "~/Library/Saved Application State/org.musicbrainz.picard.savedState",
   ]
-
 end
